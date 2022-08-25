788f00924b7fbeb82cdce8fff0d5917d387031cc
samsaddam <samsaddam14@gmail.com>
2022-08-25 23:15:42 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import '../src/assets/style.css'
+import { useSelector, useDispatch } from 'react-redux';
+import { useState } from 'react'
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const increment = () => {
+    dispatch({ type: 'INC' });
+  }
+  const decrement = () => {
+    dispatch({ type: 'DEC' });
+  }
+  const addby = () => {
+
+    dispatch({ type: 'ADD', payload: parseInt(countVal) });
+  }
+  const [countVal, setCountVal] = useState(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class='container'>
+      <div class="counter">
+        <h1>Counter App </h1>
+        <h2>{counter}</h2>
+        <div class='btn'>
+          <button onClick={increment}>Increment </button>
+          <button onClick={decrement}>Decrement</button>
+        </div>
+        <div class="add">
+          <button onClick={addby}><input type='number' placeholder='input value' onChange={(e) => setCountVal(e.target.value)} /> Add Count </button>
+        </div>
+      </div>
     </div>
   );
 }
